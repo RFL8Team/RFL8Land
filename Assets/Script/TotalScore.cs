34dfa15c1040304881f668acf22a4d0830b85181
RFLEpicGamer <69988411+RFLEpicGamer@users.noreply.github.com>
2022-03-05 20:10:51 -0300

    Import Unity Project

---
+++ Assets/Script/TotalScore.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text guiText;
+    public Text guiShadow;
+    int score = 0;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        guiText.text = "Score: " + score.ToString();
+        guiShadow.text = guiText.text;
+    }
+}
