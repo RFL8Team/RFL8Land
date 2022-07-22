545fea0226969fe50f7fc2d9b184aa018f2791e5
RFLEpicGamer <69988411+RFLEpicGamer@users.noreply.github.com>
2022-07-22 02:00:48 -0300

    The script part i almost gave up, lol

--- Player.gd
+++ Player.gd
@@ -1,34 +1,38 @@
 extends KinematicBody
 
-# How fast the player moves in meters per second.
-export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-export var fall_acceleration = 75
-export var jump_impulse = 20
+export var speed := 7.0
+export var jump_strength := 20.0
+export var gravity := 50.0
 
-var velocity = Vector3.ZERO
+var _velocity := Vector3.ZERO
+var _snap_vector := Vector3.DOWN
 
+onready var _spring_arm : SpringArm = $SpringArm
+onready var _model: Spatial = $RFL8Avatar
 
-func _physics_process(delta):
-	var direction = Vector3.ZERO
+func _physics_process(delta: float) -> void:
+	var move_direction := Vector3.ZERO
+	move_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	move_direction.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
+	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+	
+	_velocity.x = move_direction.x * speed
+	_velocity.z = move_direction.z * speed
+	_velocity.y -= gravity * delta
+	
+	var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
+	var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
+	if is_jumping:
+		_velocity.y = jump_strength
+		_snap_vector = Vector3.ZERO
+	elif just_landed:
+		_snap_vector = Vector3.DOWN
+	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	
+	if _velocity.length() > 0.2:
+		var look_direction = Vector2(_velocity.z, _velocity.x)
+		_model.rotation.y = look_direction.angle()
+		
 
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$RFL8Avatar.look_at(translation + direction, Vector3.UP)
-
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y += jump_impulse
-
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
-	velocity.y -= fall_acceleration * delta
-	velocity = move_and_slide(velocity, Vector3.UP)
+func _process(_delta: float) -> void:
+	_spring_arm.translation = translation
