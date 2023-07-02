ad3d23bc737638cfa99d7441ea6db68a1a76c088
RFLEpicGamer <rflepicgamer@vivaldi.net>
2023-07-01 23:00:49 -0300

    Organized some stuff

---
+++ Scripts/Test.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _input(ev):
+	if Input.is_action_just_pressed("return_to_title_screen"):
+		Input.mouse_mode = 0
+		get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
